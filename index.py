09db9f4fd58278d7bbe6688ae1ee9694e625f16e
mark-villanueva <villanuevamark455@gmail.com>
2024-03-16 13:30:08 +0800

    changes

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import streamlit.components.v1 as components
 
 # Function to load data from Excel file
 def load_data(file):
@@ -14,24 +13,42 @@ def display_employee_data(df, selected_employee):
 
 # Function to generate HTML for payslip
 def generate_payslip_html(payslip_date, selected_employee, employee_data):
+    if employee_data.empty:
+        return "No data available for this employee."
+
+    # Fill NaN values with empty string
+    employee_data = employee_data.fillna('')
+
     payslip_html = f"""
     <html>
     <head>
         <title>Payslip</title>
         <style>
+            @media print {{
+                @page {{
+                    size: 3in 4in;
+                    margin: 2mm;
+                }}
+                body {{
+                    color: black !important;
+                }}
+                .page-break {{
+                    page-break-before: always;
+                }}
+            }}
             body {{
                 font-family: Arial, sans-serif;
             }}
             .payslip-container {{
                 max-width: 600px;
-                margin: 0 auto;
+                margin: 0 auto ;
             }}
             .payslip-header {{
-                text-align: center;
+                margin-left: 70px;
                 margin-bottom: 20px;
             }}
             .payslip-details {{
-                margin-bottom: 20px;
+                margin-bottom: 10px;
             }}
             .payslip-item {{
                 margin-bottom: 10px;
@@ -59,59 +76,37 @@ def generate_payslip_html(payslip_date, selected_employee, employee_data):
                 </div>
             </div>
         </div>
-        <script>
-            function printPayslip() {{
-                window.print();
-            }}
-        </script>
+        <div class="page-break"></div>
     </body>
     </html>
     """
     return payslip_html
 
+
 def main():
-    st.title("Payslip Generator")
+    st.sidebar.title("Payslip Generator")
 
     # File upload
-    file = st.file_uploader("Upload Excel file", type=["xlsx"])
+    file = st.sidebar.file_uploader("Upload Excel file", type=["xlsx"])
 
     if file is not None:
         # Load data from Excel file
         df = load_data(file)
 
         # Display uploaded data
-        st.write("### Uploaded Data:")
-        st.write(df)
+        st.sidebar.write("### Uploaded Data:")
+        st.sidebar.write(df)
 
-        # Dropdown menu to select employee
         employees = df['EMPLOYEE'].unique().tolist()
-        selected_employee = st.selectbox("Select an employee", employees)
 
         # Date input for payslip date
-        payslip_date = st.date_input("Payslip Date", value=pd.Timestamp.today())
+        payslip_date = st.sidebar.date_input("Payslip Date", value=pd.Timestamp.today())
 
-        if selected_employee:
-            # Display data for selected employee
-            st.write("### Payslip for Selected Employee:")
-            employee_data = display_employee_data(df, selected_employee)
-
-            # Display payslip
-            st.markdown(f"**Date:** {payslip_date.strftime('%Y-%m-%d')}")
-            st.markdown(f"**Name:** {selected_employee}")
-            st.markdown("---")
-            st.markdown(f"**Salary:** {employee_data['SALARY'].iloc[0]} ({employee_data['DAYS'].iloc[0]} days)")
-            st.markdown(f"**Overtime:** {employee_data['OT AMOUNT'].iloc[0]}")
-            st.markdown(f"**Total:** Php {employee_data['GROSS PAY'].iloc[0]}")
-            st.markdown("---")
-            st.markdown(f"**Vale:** {employee_data['VALE'].iloc[0]}")
-            st.markdown(f"**Advance:** {employee_data['Advance'].iloc[0]}")
-            st.markdown(f"**Total Deduction:** Php {employee_data['T DED'].iloc[0]}")
-            st.markdown(f"**Take Home Pay:** Php {employee_data['THOME PAY'].iloc[0]}")
-
-            # Print button
-            if st.button('Print Payslip'):
-                payslip_html = generate_payslip_html(payslip_date, selected_employee, employee_data)
-                st.write(payslip_html, unsafe_allow_html=True)
+        if st.sidebar.button('Generate Payslips'):
+            for employee in employees:
+                employee_data = display_employee_data(df, employee)
+                payslip_html = generate_payslip_html(payslip_date, employee, employee_data)
+                st.markdown(payslip_html, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
